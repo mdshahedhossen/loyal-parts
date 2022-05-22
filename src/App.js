429e0088ceb2689acc31c13514e33698e4f8b599
mdshaedhossen <shaheddiu23@gmail.com>
2022-05-23 02:58:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import './App.css';
 
+import { Routes } from "react-router-dom";
+import Navbar from "./pages/Shared/Navbar";
+
 function App() {
   return (
-    <div>
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
+    <div className="App">
+      <Navbar></Navbar>
     </div>
   );
 }

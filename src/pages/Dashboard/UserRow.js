3c71957b7b0410e2cc628e018799938fcf61dcb2
mdshaedhossen <shaheddiu23@gmail.com>
2022-05-27 13:11:45 +0600

    user admin commit

--- src/pages/Dashboard/UserRow.js
+++ src/pages/Dashboard/UserRow.js
@@ -9,10 +9,16 @@ const UserRow = ({user,index,refetch}) => {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         },
       })
-      .then(res=>res.json())
+      .then(res=>{
+        if(res.status === 403){
+          toast.error('Failed to Make an admin');
+      }
+      })
       .then(data=>{
-        refetch();
-       toast.success(`Successfully made an admin`);
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Successfully made an admin`);
+      }
       })
     }
     return (
